1c3d47b36494caf89e275ed03e22d5c914727984
YevgeniyGertsen <gersen.e.a@gmail.com>
2025-09-16 09:47:36 +0500

    Добавил передачу данных в контроллер

---
+++ Hotel.ATR.Web.First/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Web.First.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveData(string name, string email, string message)
+        {
+
+            //write to DataBase
+            
+            return View();
+        }
+    }
+}
