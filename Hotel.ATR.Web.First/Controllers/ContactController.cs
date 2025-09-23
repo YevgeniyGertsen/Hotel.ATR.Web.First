83d92aef47fef6dd0e2a9e146c6ed98b60d4f637
YevgeniyGertsen <gersen.e.a@gmail.com>
2025-09-23 08:57:13 +0500

    Последние измнененя

--- Hotel.ATR.Web.First/Controllers/ContactController.cs
+++ Hotel.ATR.Web.First/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.ATR.Web.First.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.ATR.Web.First.Controllers
 {
@@ -6,16 +7,33 @@ namespace Hotel.ATR.Web.First.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            ContactForm form = new ContactForm();
+            form.name = "Yevgeniy";
+            form.email = "gersen.e.a@gmail.com";
+
+            return View(form);
         }
 
         [HttpPost]
-        public IActionResult SaveData(string name, string email, string message)
+        public IActionResult SaveData(ContactForm form)
         {
+            //1
+            //string name, string email, string message
+
+            //2
+            var _name = Request.Form["name"];
+            var _email = Request.Form["email"];
+            var _message = Request.Form["message"];
+
+            //3
 
             //write to DataBase
-            
-            return View();
+
+            ViewBag.Result = "Ваше сообщение отправлено!";
+            TempData["Result"] = "Ваше сообщение отправлено!";
+
+            return RedirectToAction("Index");
+            //return View();
         }
     }
 }
