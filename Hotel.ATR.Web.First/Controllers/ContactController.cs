e44f33ba9cc0e5a0a31dff549bfa35d76f5a8392
YevgeniyGertsen <gersen.e.a@gmail.com>
2025-09-23 09:28:30 +0500

    Добавил валидацияю

--- Hotel.ATR.Web.First/Controllers/ContactController.cs
+++ Hotel.ATR.Web.First/Controllers/ContactController.cs
@@ -5,35 +5,44 @@ namespace Hotel.ATR.Web.First.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(ContactForm contactForm = null)
         {
-            ContactForm form = new ContactForm();
-            form.name = "Yevgeniy";
-            form.email = "gersen.e.a@gmail.com";
-
-            return View(form);
+            return View(contactForm);
         }
 
         [HttpPost]
         public IActionResult SaveData(ContactForm form)
         {
+            if (string.IsNullOrWhiteSpace(form.name))
+                ModelState.AddModelError("name", "Укажите свое имя");
+
+            #region old
             //1
             //string name, string email, string message
 
             //2
-            var _name = Request.Form["name"];
-            var _email = Request.Form["email"];
-            var _message = Request.Form["message"];
+            //var _name = Request.Form["name"];
+            //var _email = Request.Form["email"];
+            //var _message = Request.Form["message"];
 
             //3
 
             //write to DataBase
 
-            ViewBag.Result = "Ваше сообщение отправлено!";
-            TempData["Result"] = "Ваше сообщение отправлено!";
+            //ViewBag.Result = "Ваше сообщение отправлено!";
+            //TempData["Result"] = "Ваше сообщение отправлено!";
 
             //return RedirectToAction("Index");
-            return View();
+            #endregion
+
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", new { contactForm = form });
+            }           
         }
     }
 }
